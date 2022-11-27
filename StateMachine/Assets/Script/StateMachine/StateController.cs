0096f1fdd1d26f11b6f7dde94a1217207703c65a
Sara <sbolognesi95@gmail.com>
2022-11-27 18:48:49 +0100

    Added basic state machine script

---
+++ StateMachine/Assets/Script/StateMachine/StateController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+	public abstract class StateController : MonoBehaviour
+	{
+		public State currentState;
+		public float currentStateTime;
+		protected bool freeze;
+		private float timeIsFreezing;
+			
+		
+		public void Update()
+		{
+			
+				currentState.UpdateState(this);
+				currentStateTime += Time.deltaTime;
+			
+			
+		}
+
+		public void TransitionToState(State nextState)
+		{
+			if (currentState == nextState)
+				return;
+
+			currentStateTime = 0;
+			currentState = nextState;
+		}
+
+		
+	}
